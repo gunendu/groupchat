b11c0d46779cbf55cb27443a292fb8d765d92903
gunendu <gunendu@gmail.com>
2016-12-21 02:03:07 +0530

    group chat changes

--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,14 @@ module.exports = function(io){
   io.sockets.on('connection', function (socket) {
 
   chat.find({},{message:1},function(err,chats){
-    console.log(usernames,socket.username);
     _.each(chats,function(chat){
-        io.sockets.emit('updatechat',chat.message);
+        io.sockets.to(socket.id).emit('updatechat',chat.message);
     })
   })
 
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-    var chatObj = new chat({
+		var chatObj = new chat({
       "message":data
     })
     chatObj.save(function(err){
@@ -37,26 +35,17 @@ module.exports = function(io){
 
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
-		// we store the username in the socket session for this client
 		socket.username = username;
-		// add the client's username to the global list
 		usernames[username] = username;
-		// echo to client they've connected
+
 		socket.emit('updatechat', 'SERVER', 'you have connected');
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-		// update the list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
 	});
 
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function(){
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// update list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER has disconnected');
+		//socket.broadcast.emit('updatechat', 'SERVER has disconnected');
 	});
 });
 
